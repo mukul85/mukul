ff54998e769239bc63b6a66b4a6195bae688f43e
mukul85 <67535625+mukul85@users.noreply.github.com>
2020-09-13 10:12:04 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,5 +1,3 @@
-"""Check if userbot alive. If you change these, you become the gayest gay such that even the gay world will disown you."""
-#IMG CREDITS: @WhySooSerious
 import asyncio
 from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
@@ -9,19 +7,17 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-ALIVE_IMG = "https://telegra.ph/file/6225d447ed8ba1d23079f.jpg"
-ALIVE_caption = "`Sensible Userbot IS:` **ONLINE**\n\n"
+ALIVE_IMG = ""
+ALIVE_caption = "`Mukul's private Userbot IS:` **ONLINE**\n\n"
 ALIVE_caption += "**SYSTEM STATUS**\n\n"
 ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
-ALIVE_caption += "**Sensible  OS** : `3.14`\n\n"
+ALIVE_caption += "**mukul's private  OS** : `3.14`\n\n"
 ALIVE_caption += "**Current Sat** : `Sensible Userbot Sat-2.95`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**Bot Made By @ceowhitehatcracks \n\n"
-ALIVE_caption += "Copyright By [CEOWHITEHATCRACKS](GitHub.com/spandey112)\n\n"
-ALIVE_caption += "[Deploy SensibleUserbot](GitHub.com/spandey112/SensibleUserbot)"
+ALIVE_caption += "**Bot Made By @mukuljain10 \n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
