6015e1c487be035b0b004fdd2d554ee0955cf9e9
mukul85 <67535625+mukul85@users.noreply.github.com>
2020-09-13 19:28:41 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -14,10 +14,10 @@ ALIVE_caption += "`TELETHON VERSION:` **6.0.9**\n`Python:` **3.7.4**\n\n"
 ALIVE_caption += "`DATABASE STATUS:` **Functional**\n\n"
 ALIVE_caption += "**Current Branch** : `master`\n\n"
 ALIVE_caption += "**mukul's private  OS** : `3.14`\n\n"
-ALIVE_caption += "**Current Sat** : `Sensible Userbot Sat-2.95`\n\n"
+ALIVE_caption += "**Current Sat** : `Mukul's private Userbot Sat-2.95`\n\n"
 ALIVE_caption += f"**My Boss** : {DEFAULTUSER} \n\n"
 ALIVE_caption += "**Heroku Database** : `AWS - Working Properly`\n\n"
-ALIVE_caption += "**Bot Made By @mukuljain10 \n\n"
+ALIVE_caption += "**Bot Made By @mukuljain10 **\n\n"
 #@command(outgoing=True, pattern="^.alive$")
 @borg.on(admin_cmd(pattern=r"alive"))
 async def amireallyalive(alive):
