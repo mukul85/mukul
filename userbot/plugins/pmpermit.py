1c50ba204f94de775575267076da223766e28801
mukul85 <67535625+mukul85@users.noreply.github.com>
2020-09-15 21:14:36 +0530

    Update pmpermit.py

--- userbot/plugins/pmpermit.py
+++ userbot/plugins/pmpermit.py
@@ -203,6 +203,7 @@ if Var.PRIVATE_GROUP_ID is not None:
                 )
                 return
             except:
+                return
         r = await event.client.send_file(event.chat_id, WARN_PIC, caption=USER_BOT_NO_WARN)
         PM_WARNS[chat_id] += 1
         if chat_id in PREV_REPLY_MESSAGE:
